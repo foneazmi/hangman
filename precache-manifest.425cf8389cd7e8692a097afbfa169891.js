52f53c079897149c842df4bec9c316ebd948f468
Farkhan <farkhan>
2020-08-18 01:14:25 +0700

    Updates

---
+++ precache-manifest.425cf8389cd7e8692a097afbfa169891.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af53f2d922c8a5a0c596a74a7dc1904f",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "a754c185020298e99d3b",
+    "url": "/hangman/static/css/main.896e1964.chunk.css"
+  },
+  {
+    "revision": "973e31a4495480d1933b",
+    "url": "/hangman/static/js/2.5da62bfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.5da62bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a754c185020298e99d3b",
+    "url": "/hangman/static/js/main.1ca90c10.chunk.js"
+  },
+  {
+    "revision": "7ef95293683d0ed87720",
+    "url": "/hangman/static/js/runtime-main.6b115b9b.js"
+  }
+]);
